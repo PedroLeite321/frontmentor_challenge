4d3e25b5eeac363b7f9e82706d5e51a31c2c8085
pedro <pk.bleite@gmail.com>
2024-02-18 14:58:20 -0300

    working on mobile landscape view / trabalhando na visão de landscape mobile

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,26 +1,56 @@
 //teste
 const loadSucessPage = () =>    {
+    let alertMsg;
+    
     const states = {
         button: document.querySelector('.submit_btn_main'),
         dismissBtn: document.querySelector(".dismiss"),
+        emailInput: document.querySelector("#mail"),
+        alertNoInput: document.querySelector(".alert_message"),
             
     
         
     };
 
-    states.button.addEventListener('click', (e) =>   {
-        e.preventDefault();
-        window.location.href = "../../src/pages/thanks.html";
-    });
-
+    states.alertNoInput.innerHTML ==   alertMsg;
     
-    states.dismissBtn.addEventListener('click', (e) =>   {
-        e.preventDefault();
-        window.location.href = "../../index.html";
+    const checkRequired = () => {
+        
+   
+
+    states.button.addEventListener('click', (e) =>   {        
+   
+        if(states.emailInput.value === "" || states.emailInput.value  === undefined || states.emailInput.value === null)    {
+            states.alertNoInput.innerHTML = "Email is Empty";
+            alertMsg = states.alertNoInput.innerHTML;
+
+        }else if(states.emailInput.value.includes("@") === false)   {
+            states.alertNoInput.innerHTML = "Valid email required";
+            alertMsg = states.alertNoInput.innerHTML;
+        }
+        
+        else{
+            states.alertNoInput.innerHTML = "";
+            e.preventDefault();
+            window.location.href = "../../src/pages/thanks.html";
+            
+        }
     });
 
-    console.log(states.button);
-    console.log(states.dismissBtn)
+
+   
+   
+   
+        
+        
+
+        
+   }
+   
+   checkRequired();
 
 }
 loadSucessPage();
+
+
+
